7dfcc1f9bf2d27e682e8bca5c5275351e5788951
Adam Draper <adamwdraper@gmail.com>
2025-07-06 14:42:03 -0700

    remvoe registry

--- narrator/__init__.py
+++ narrator/__init__.py
@@ -7,15 +7,6 @@ from .storage.file_store import FileStore
 from .models.thread import Thread
 from .models.message import Message
 from .models.attachment import Attachment
-from .utils.registry import (
-    register_thread_store,
-    get_thread_store,
-    register_file_store,
-    get_file_store,
-    register,
-    get,
-    list as list_registered_components,
-)
 
 __version__ = "0.1.0"
 __all__ = [
@@ -24,11 +15,4 @@ __all__ = [
     "Thread",
     "Message",
     "Attachment",
-    "register_thread_store",
-    "get_thread_store",
-    "register_file_store",
-    "get_file_store",
-    "register",
-    "get",
-    "list_registered_components",
 ] 
--- narrator/utils/__init__.py
+++ narrator/utils/__init__.py
@@ -3,23 +3,7 @@ Utilities package for Tyler Stores
 """
 
 from .logging import get_logger
-from .registry import (
-    register_thread_store,
-    get_thread_store,
-    register_file_store,
-    get_file_store,
-    register,
-    get,
-    list as list_registered_components,
-)
 
 __all__ = [
     "get_logger",
-    "register_thread_store",
-    "get_thread_store",
-    "register_file_store",
-    "get_file_store",
-    "register",
-    "get",
-    "list_registered_components",
 ] 
