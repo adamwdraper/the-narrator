3bdfdb25a542fdc49b3cd09e679ea94448da388f
Adam Draper <adamwdraper@gmail.com>
2025-07-06 17:21:57 -0700

    Bump version to Updated version in pyproject.toml Updated version in narrator/__init__.py Version bumped from 0.1.0 to 0.1.1

--- narrator/__init__.py
+++ narrator/__init__.py
@@ -8,7 +8,7 @@ from .models.thread import Thread
 from .models.message import Message
 from .models.attachment import Attachment
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = [
     "ThreadStore",
     "FileStore", 
